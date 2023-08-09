436fa912a7c7b72a88172067c57da91a4921ba85
Sai Aswin Madhavan <saiaswin.211ec244@nitk.edu.in>
2023-08-09 23:48:10 +0530

    Delete app directory

--- app/blueprints/courses/courses.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, redirect
-
-courses_bp = Blueprint("courses", __name__, template_folder="templates",static_folder="static")
-
-@courses_bp.route("/")
-def courses():
-    return "Courses Page"
--- app/blueprints/timetable/timetable.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, redirect
-
-timetable_bp = Blueprint("timetable", __name__, template_folder="templates",static_folder="static")
-
-@timetable_bp.route("/")
-def timetable():
-    return "TimeTable Page"
